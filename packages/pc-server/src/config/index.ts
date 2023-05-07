5f905aee38e6873df602d3fdb20f1a044e3bae73
calnong <2441415772@qq.com>
2023-05-07 21:38:40 +0800

    feat: 更新项目结构说明

--- packages/pc-server/src/config/index.ts
+++
@@ -1,7 +0,0 @@
-import type { TypeOrmModuleOptions } from '@nestjs/typeorm';
-
-// import { RedisModuleOptions } from '@';
-
-export type DBConfig = TypeOrmModuleOptions;
-
-// export type RedisConfig =
